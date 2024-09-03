7e37d661e0ba183f5e2e01ecae2ce010f933a93d
Andrew Kang <akang275@gmail.com>
2024-09-03 15:25:34 -0400

    finish static site generator

--- insta485generator/__main__.py
+++ insta485generator/__main__.py
@@ -8,7 +8,7 @@ import shutil
 @click.command()
 @click.argument("input_dir", nargs=1, type=click.Path(exists=True))
 @click.option("-o", "--output", type=click.Path(), help="Output directory.")
-@click.option("-v", "--verbose", help="Print more output.")
+# @click.option("-v", "--verbose", help="Print more output.")
 def main(input_dir, output):
     """Templated static website generator"""
 
@@ -16,21 +16,27 @@ def main(input_dir, output):
     config_filename = input_dir / 'config.json'
     template_dir = input_dir / "templates"
 
-    with config_filename.open() as config_file:
-        config_data = json.load(config_file)
+    try:
+        with config_filename.open() as config_file:
+            config_data = json.load(config_file)
+    except FileNotFoundError as e:
+        print(f"insta485generator error: '{config_filename}' not found")
+        exit(1)
 
     for page in config_data:
-        url = pathlib.Path(page["url"].lstrip("/"))
-        # template = template_dir / page["template"]
-        output_dir = input_dir / "html" / url #default
+        output_dir = input_dir / "html" / page["url"].lstrip("/")
         if output:
             output_dir = pathlib.Path(output)
+        try:
+            output_dir.mkdir(parents=True, exist_ok=False)
+        except FileExistsError as e:
+            print(f"insta485generator error: '{output}' already exists")
+            exit(1)
+
         static_dir = input_dir/"static"
         if static_dir.is_dir():
-            shutil.copytree(static_dir, output_dir, dirs_exist_ok=False)
-        else:
-            output_dir.mkdir(parents=True, exist_ok=True)
-
+            shutil.copytree(static_dir, output_dir, dirs_exist_ok=True)
+        
         template_env = jinja2.Environment(
             loader=jinja2.FileSystemLoader(str(template_dir)),
             autoescape=jinja2.select_autoescape(['html', 'xml']),
